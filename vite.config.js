7248d887e22aefb46a8d15db755a5ca14c640c40
mahen497 <patilmahendra497@gmail.com>
2025-10-07 11:24:58 +0530

    git automation

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// 👇 Add base for GitHub Pages (important!)
 export default defineConfig({
+  base: '/pmd/',
   plugins: [react(), tailwindcss()],
 });
